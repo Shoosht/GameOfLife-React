2ac16a71a89d94be058ab70f5cd141bd5f2615e5
Shoosht <115585203+Shoosht@users.noreply.github.com>
2023-07-13 00:00:19 +0200

    Added Show/Hide Navbar button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,11 +6,12 @@ class Navbar extends Component {
     super(props);
     this.state = {
       startText: this.props.startText,
+      hideShow: 'Show',
     };
   }
 
   changeStartText() {
-    const { startText } = this.state;
+    const startText = this.state.startText;
     if (startText === 'Start') {
       this.setState({ startText: 'Stop' });
       this.props.setStartText('Stop');
@@ -32,25 +33,42 @@ class Navbar extends Component {
     this.props.setRestart(!this.props.restart)
   }
 
+  showHideNavbar(){
+    const hideShow = this.state.hideShow;
+    if(hideShow === 'Show'){
+      this.setState({hideShow: 'Hide'});
+    }else if (hideShow === 'Hide'){
+      this.setState({hideShow: 'Show'});
+    }
+  }
+
   render() {
-    const { startText } = this.state;
+    const { startText, hideShow } = this.state;
 
     return (
       <div>
-        <div className="navbar">
-          <div className="left">
-            <button onClick={() => this.changeStartText()} className="button">
+        <div className={`navbar-${hideShow}`}>
+          <div className={`left-${hideShow}`}>
+            <button className="button" onClick={() => this.changeStartText()}>
               {startText}
             </button>
           </div>
           <div className="middle">
           </div>
-          <div className="right">
-            <button onClick={() => this.restartGrid()} className="button">
+          <div className={`right-${hideShow}`}>
+            <button className="button" onClick={() => this.restartGrid()}>
               Restart
             </button>
           </div>
         </div>
+        {(hideShow === 'Hide' || hideShow === 'Show') && (
+            <div className="showhide-container">
+              <div className={hideShow} onClick={() => this.showHideNavbar()}>
+                <div className={`${hideShow}-arrow`}>
+                </div>
+              </div>
+            </div>
+            )}
       </div>
     );
   }
